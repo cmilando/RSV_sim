d0a22d51528002f1ce2f86424638f8de4c5324b1
Chad Milando <44262671+cmilando@users.noreply.github.com>
2026-03-05 10:47:06 -0500

    works

--- fortranTest.R
+++ fortranTest.R
@@ -29,19 +29,36 @@ oo <- .Fortran("ransam",
 # have to pass in the thing you want to get out
 # df, nrows, ncols, age_col, age_lb, age_ub, rr
 set.seed(123)
-nrows = as.integer(100)
+nrows = as.integer(10)
 ncols = as.integer(4)
-df <- matrix(rnorm(100 * 4), nrow = nrows, ncol = ncols)
-df[sample(1:nrows, 20), 3] = 0
+df <- matrix(rnorm(nrows * ncols), nrow = nrows, ncol = ncols)
+
+# set id
 df[, 1] <- 100 + 1:nrows
+df
+
+# set age
 age_col = as.integer(2)
-age_lb = 0
-age_ub = 1
+df[, 2] <- c(rep(20, 5), rep(50, 5))
+df
+age_lb = 10
+age_ub = 25
+df
+
+# set zerocol
+zero_col = as.integer(3)
+df[, 3] <- 0
+df
+
+# df[sample(1:nrows, 20), 3] = 0
 rr = rep(as.integer(0), times = nrows)
-n = as.integer(20)
+n = as.integer(5)
 xcontinue = 1
-ids <- rnorm(100)
-ids_size <- as.integer(100)
+ids <- rnorm(nrows)
+ids_size <- as.integer(nrows)
+
+df
+ids
 
 # remember that the most common cause of failure and abort
 # is misconfiguration of inputs in this call
@@ -50,27 +67,40 @@ ids_size <- as.integer(100)
 # -- or its real when it should be an integer
 # -- or the order isn't correct
 oo <- .Fortran("get_ids",
+               df        = df,
+               nrows     = nrows,
+               ncols     = ncols,
+               age_col   = age_col,
+               age_lb    = age_lb,
+               age_ub    = age_ub,
+               zero_col  = zero_col,
+               rr        = rr,
+               n         = n,
+               xcontinue = xcontinue,
+               ids       = ids,
+               ids_size  = ids_size)
+
+oo$rr  # things that match
+oo$ids # this group
+oo$ids_size
+table(oo$rr)
+df[which(oo$rr == 1),]
+
+
+## OK FINALLY
+# df, nrows, ncols,  age_col, vec, zero_col, &
+#   & p1, p2, age0, age1, age2
+oo <- .Fortran("set_ids",
                df = df,
                nrows = nrows,
                ncols = ncols,
                age_col = age_col,
-               age_lb = age_lb,
-               age_ub = age_ub,
+               vec = as.integer(rep(5, nrows)),
                zero_col = as.integer(3),
-               rr = rr,
-               n = n,
-               xcontinue = xcontinue,
-               ids = ids,
-               ids_size = ids_size)
+               p1 = 0.2,
+               p2 = 0.8,
+               age0 = 0,
+               age1 = 30,
+               age2 = 125)
 
-cat("IDS\n")
-oo$ids
-
-cat("IDsize\n")
-oo$ids_size
-
-cat("rr\n")
-oo$rr
-
-table(oo$rr)
-df[which(oo$rr == 1),]
+oo$df
--- rsv.f90
+++ rsv.f90
@@ -10,6 +10,168 @@
 !--------------------------------------------------------------------------
 
 !--------------------------------------------------------------------------
+! Subroutine for set_ids
+!--------------------------------------------------------------------------
+subroutine set_ids(df, nrows, ncols,  age_col, vec, zero_col, &
+                    & p1, p2, age0, age1, age2)
+
+    !# **********
+    !# local_pop_df = pop_df
+    !# vec = household_sizes
+    !# column = "household_id"
+    !# p1 = 0.5; p2 = 0.5
+    !# age0 = 0; age1 = 25; age2 = 125
+    !***********************
+
+    implicit none
+
+
+    ! =========================================================================
+    ! /////////////////////////////////////////////////////////////////////////
+    ! VARIABLE DEFINTIONS
+    ! /////////////////////////////////////////////////////////////////////////
+    ! =========================================================================
+
+    integer, intent(in)       :: nrows     ! the number of rows of df
+    integer, intent(in)       :: ncols     ! the number of columns of df
+    integer, intent(in)       :: age_col   ! which columns is the age_column
+    integer, intent(in)       :: zero_col  ! which column is currently being sorted on
+    real(kind=8)              :: df(nrows, ncols) ! a real matrix
+    integer, intent(in)       :: vec(nrows) ! a vector of group sizes
+    real(kind=8), intent(in)  :: p1, p2   ! probabilities of the age groups
+    real(kind=8), intent(in)  :: age0, age1, age2 ! age group cutoffs
+
+    ! helpers
+    integer                   :: xcontinue, not_all_assigned
+    integer                   :: ii, k
+    integer                   :: total_grp_size, grp1_size, grp2_size
+
+    ! blanks to pass to get_ids
+    integer                   :: rr(nrows)  ! which rows to be selected
+    real(kind=8)              :: ids(nrows) ! an array of ids, passed in a reset to 0
+    integer                   :: ids_size   ! how many to count forward in ids
+    integer                   :: n1, n2     ! how many in each group
+
+    ! initialize
+    ii = 1
+    xcontinue = 1
+
+    ! =========================================================================
+    ! /////////////////////////////////////////////////////////////////////////
+    ! START THE LOOP
+    ! since its a while continue, just use line number statements
+    ! /////////////////////////////////////////////////////////////////////////
+    ! =========================================================================
+
+40  if(mod(ii, 1000) .eq. 0) then
+      write(*,*) ii
+    end if
+
+    ! set this group size
+    total_grp_size = vec(ii)
+
+    ! get group sizes
+    grp1_size = floor(p1 * float(total_grp_size))
+    grp2_size = ceiling(p2 * float(total_grp_size))
+
+    ! check split math
+    if((total_grp_size) .NE. (grp1_size + grp2_size)) then
+      write(*,*) 'error in split math'
+      return
+    end if
+
+    ! initialize
+    ! need to define rr, ids, and ids_size
+    rr(:)     = 0
+    ids_size  = 0
+    ids(:)    = 0
+    n1        = INT(0)
+    xcontinue = 1
+
+    ! get ids for group 1
+    if(grp1_size .gt. 0) then
+
+
+      call get_ids(df, nrows, ncols, age_col, age0, age1, zero_col, rr, &
+                 & grp1_size, xcontinue, ids, ids_size)
+
+      n1 = ids_size
+      ! write(*,*) "g1 xcontinue", xcontinue
+
+    end if
+
+    ! now continue for the 2nd group
+    ! initialize again
+    rr(:)     = 0
+    ids_size  = 0
+    ids(:)    = 0
+
+    ! xcontinue 1
+    if(xcontinue .eq. 1) then
+
+      call get_ids(df, nrows, ncols, age_col, age1, age2 + 1, zero_col, rr, &
+           & grp2_size, xcontinue, ids, ids_size)
+
+      n2 = ids_size
+      ! write(*,*) "g2 xcontinue", xcontinue
+
+      ! xcontinue 2
+      if(xcontinue .eq. 1) then
+
+        ! essentially its the ones that are -1
+        do k = 1, nrows
+          if (df(k, zero_col) .eq. -1.0) then
+             df(k, zero_col) = float(ii)
+          end if
+        end do
+
+        ii = ii + 1
+
+        ! define the stopping conditions
+        not_all_assigned = 0
+        do k = 1, nrows
+          if (df(k, zero_col) .le. 0.0) then
+             not_all_assigned = 1
+          end if
+        end do
+        if(not_all_assigned .eq. 0) then
+          write(*,*) "all ids are complete - stopping"
+          xcontinue = 0
+        end if
+
+      else
+
+        write(*,*) "out2 continue is FALSE -", total_grp_size, n1, n2
+        xcontinue = 0
+
+      end if ! 2
+
+    else
+
+      write(*,*) "out1 continue is FALSE -",n1
+      xcontinue = 0
+
+    end if ! 1
+
+    if (xcontinue .eq. 1) goto 40
+
+
+    ! =========================================================================
+    ! /////////////////////////////////////////////////////////////////////////
+    ! OUTPUT
+    ! /////////////////////////////////////////////////////////////////////////
+    ! =========================================================================
+    write(*,*) "Number of groups:", ii
+    write(*,*) "Last group size:", vec(ii)
+    do k = 1, nrows
+      if (df(k, zero_col) .le. 0.0) then
+         df(k, zero_col) = -999.99
+      end if
+    end do
+
+end subroutine set_ids
+
+!--------------------------------------------------------------------------
 ! Subroutine for get_ids
 !--------------------------------------------------------------------------
 subroutine get_ids(df, nrows, ncols, age_col, age_lb, age_ub, zero_col, rr, &
@@ -229,7 +391,7 @@ subroutine fsubset(df, nrows, ncols, age_col, age_lb, age_ub, zero_col, rr)
 
     ! iterate
     do i = 1, nrows
-      if(df(i, age_col) >= age_lb .and. df(i, age_col) < age_ub &
+      if(df(i, age_col) .ge. age_lb .and. df(i, age_col) .lt. age_ub &
       & .and. df(i, zero_col) == 0) then
         rr(i) = INT(1)
       end if
